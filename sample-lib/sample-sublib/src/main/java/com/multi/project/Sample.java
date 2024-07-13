131d56ccb6095cc2316ed7c8943a8371a52e7ba0
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2024-07-12 23:13:19 -0500

    change

---
+++ sample-lib/sample-sublib/src/main/java/com/multi/project/Sample.java
@@ -0,0 +1,13 @@
+package com.multi.project;
+
+public class Sample {
+    private String value;
+
+    public Sample(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
